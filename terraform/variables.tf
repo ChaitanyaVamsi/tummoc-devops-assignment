273ffb7f9666747a6a077ceadce192b3b87b8635
chaitanya <vamsi.blaze1@gmail.com>
2026-03-25 12:16:00 +0530

    feature: added subnets

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "project" {
   type = string
-  default = "cyberpunk-chat-app"
+  default = "realtime-chatApp"
 }
 
 variable "environment" {
@@ -8,7 +8,23 @@ variable "environment" {
   default = "dev"
 }
 
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
 variable "vpc_cidr" {
   type = string
   default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  type=string
+  default = "10.0.1.0/24"
+}
+
+
+variable "private_subnet_cidr" {
+  type=string
+  default = "10.0.2.0/24"
 }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,8 +1,42 @@
+# vpc
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support = true
   tags = {
-    Name = "${var.project}-${var.environment}-vpc"
+    Name = "${local.common_name}-vpc"
+  }
+}
+
+# IGW
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+    tags = {
+    Name = "${local.common_name}-igw"
+  }
+}
+
+# subnets
+# Public subnet - ALB,NAT,Bastion
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidr
+  availability_zone = "${var.region}a"
+  map_public_ip_on_launch = true
+  tags ={
+    Name = "${local.common_name}-public-subnet"
+  }
+
+}
+
+# private subent - app EC2 , no internet
+
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidr
+  availability_zone = "${var.region}a"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "${local.common_name}-private-subnet"
   }
 }

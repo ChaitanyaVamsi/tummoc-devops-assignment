7d82e3cb30272691537cfe4575e82bc6c309bde5
chaitanya <vamsi.blaze1@gmail.com>
2026-03-25 19:30:01 +0530

    feature: updated ec2.tf

--- terraform/50-ec2.tf
+++ terraform/50-ec2.tf
@@ -38,6 +38,7 @@ resource "aws_instance" "app" {
   subnet_id = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.app.id]
   associate_public_ip_address = false
+  key_name = var.key_pair_name # if this is added we cannot ssh to this
   root_block_device {
     volume_type = "gp3"
     volume_size = 20

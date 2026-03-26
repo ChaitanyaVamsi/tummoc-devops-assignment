c1cb3caf88be1622d24946fa2ea339388c30e023
chaitanya <vamsi.blaze1@gmail.com>
2026-03-26 13:14:27 +0530

    feature: updated setup.sh

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -20,4 +20,16 @@
       -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 
+    sudo apt install npm -y
+    sudo apt install openjdk-21-jre-headless -y
+    sudo apt update
+
+    #aws cli
+    sudo apt install -y unzip curl
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
+    rm -rf aws awscliv2.zip
+    aws --version
+
     echo "Bootstrap complete" >> /var/log/user-data.log
